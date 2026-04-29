c2c343d169dbd42d2ada9e230bf205fd52650ae3
Dinker Malhotra <dinker.kis@gmail.com>
2026-04-29 16:33:35 +0530

    Recreated missing file

---
+++ ECommerce App/Sources/Helpers/Extensions/UIView+Extension.swift
@@ -0,0 +1,79 @@
+import UIKit
+
+// MARK: - UIVIEW EXTENSION
+extension UIView {
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        } set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        } set {
+            layer.cornerRadius = adjustedForDevice(newValue)
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            } else {
+                return nil
+            }
+        } set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            } else {
+                return nil
+            }
+        } set {
+            layer.shadowColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        } set {
+            layer.shadowRadius = newValue
+            layer.masksToBounds = false
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        } set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        } set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    // MARK: - Device Type–Aware Scaling
+    private func adjustedForDevice(_ value: CGFloat) -> CGFloat {
+        switch UIDevice.current.userInterfaceIdiom {
+        case .pad:
+            return value * 1.5
+        default:
+            return value
+        }
+    }
+}
